2b014c4375417efab7f230de3369c4ffcde0925c
Aleksandr Gur <aleksandrgur@gmail.com>
2015-11-09 15:07:34 +0200

    fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,5 +13,9 @@ abstract class Controller extends BaseController
     public function __construct()
     {
         header('Access-Control-Allow-Origin: *');
+
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+
+        header("Access-Control-Allow-Headers: X-Requested-With");
     }
 }
